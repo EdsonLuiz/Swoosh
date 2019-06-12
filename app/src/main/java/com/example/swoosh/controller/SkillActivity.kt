f15114d2a68b70f1aa018e6475eb32be21f97a5b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-06-12 14:36:06 -0300

    Add onClick and function boilerplate for each onCLick

--- app/src/main/java/com/example/swoosh/controller/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.example.swoosh.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.swoosh.R
 import com.example.swoosh.utilities.EXTRA_LEAGUE
 
@@ -14,4 +15,8 @@ class SkillActivity : AppCompatActivity() {
         setContentView(R.layout.activity_skill)
         leagueExtra = intent.getStringExtra(EXTRA_LEAGUE)
     }
+
+    fun onBegginerClicked(view: View) {}
+    fun onBallerClicked(view: View) {}
+    fun onSkillFinishClicked(view: View) {}
 }
