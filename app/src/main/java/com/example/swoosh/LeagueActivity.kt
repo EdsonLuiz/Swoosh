9e4a9b79884e8299febbd01ba2912d0839189f4f
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-06-12 10:49:36 -0300

    Add navigation from activity_league to activity_skill
    Implemented with onCLick calling function leagueNextClicked

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
