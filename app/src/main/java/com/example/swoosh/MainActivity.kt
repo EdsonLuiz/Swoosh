a74262b20cfd891bbd840f9bdff77457affdcba1
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-06-12 10:31:35 -0300

    Rename navigation button ids

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonFinish.setOnClickListener {
+        buttonStart.setOnClickListener {
             val leagueIntent = Intent(this, LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
