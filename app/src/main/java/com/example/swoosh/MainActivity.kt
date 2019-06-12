d982bd7e5ba5760b2ebfddd01d30794c4787d74a
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-06-12 10:35:28 -0300

    Edit import

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_league.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
