08f04a4803573d5c47b80c178a88e3a28a84f258
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-06-12 15:09:27 -0300

    Add message to FinishActivity based on EXTRA received

--- app/src/main/java/com/example/swoosh/controller/FinishActivity.kt
+++ app/src/main/java/com/example/swoosh/controller/FinishActivity.kt
@@ -1,13 +1,22 @@
 package com.example.swoosh.controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.swoosh.R
+import com.example.swoosh.utilities.EXTRA_LEAGUE
+import com.example.swoosh.utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        var skill = intent.getStringExtra(EXTRA_SKILL)
+
+        textSearchLeagues.text = "Looking for a $league $skill league near you"
     }
 }
